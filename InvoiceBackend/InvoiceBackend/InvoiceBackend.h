fecd16b210987f4f128a604941f679fb5b33bf1e
naresh-pt6259 <nareshkumaroviliraja2002@gmail.com>
2023-04-28 13:10:49 +0530

    File Init

---
+++ InvoiceBackend/InvoiceBackend/InvoiceBackend.h
@@ -0,0 +1,18 @@
+//
+//  InvoiceBackend.h
+//  InvoiceBackend
+//
+//  Created by naresh-pt6259 on 28/04/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for InvoiceBackend.
+FOUNDATION_EXPORT double InvoiceBackendVersionNumber;
+
+//! Project version string for InvoiceBackend.
+FOUNDATION_EXPORT const unsigned char InvoiceBackendVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InvoiceBackend/PublicHeader.h>
+
+
